4b1d1c98749c4f833c8568fe1839db0d486ef85b
ghubstan <36207203+ghubstan@users.noreply.github.com>
2022-03-19 13:41:56 -0300

    Leave the .proto download dir clean

--- proto-downloader/download-bisq-protos.sh
+++ proto-downloader/download-bisq-protos.sh
@@ -7,6 +7,8 @@ echo "You may want to skip this step and copy your own local .proto files instea
 curl -o ./pb.proto https://raw.githubusercontent.com/bisq-network/bisq/master/proto/src/main/proto/pb.proto
 curl -o ./grpc.proto https://raw.githubusercontent.com/bisq-network/bisq/master/proto/src/main/proto/grpc.proto
 
-# Copy the proto files to the Java and Python example projects.
+echo "Copying proto files to the Java examples project."
 cp -v *.proto ../java-examples/src/main/proto
-cp -v *.proto ../python-examples/proto
+
+echo "Moving proto files to the Python examples project;  removing .proto files in this download directory."
+mv -v *.proto ../python-examples/proto
