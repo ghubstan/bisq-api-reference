ffe277a596340dce618defa85565e839e0d65e4a
ghubstan <36207203+ghubstan@users.noreply.github.com>
2022-08-10 11:49:33 -0300

    Add --amount parameter to takeoffer examples

--- cli-examples/TakeOffer.sh
+++ cli-examples/TakeOffer.sh
@@ -2,11 +2,13 @@
 source "env.sh"
 
 # Take a BSQ swap offer.
-$BISQ_HOME/bisq-cli --password=xyz --port=9998 takeoffer --offer-id=8368b2e2-anb6-4ty9-ab09-3ebdk34f2aea
+# The amount param is optional.
+$BISQ_HOME/bisq-cli --password=xyz --port=9998 takeoffer --offer-id=8368b2e2-anb6-4ty9-ab09-3ebdk34f2aea --amount=0.1
 
 # Take an offer that is not a BSQ swap offer.
-# The payment-account-id param is required, the fee-currency param is optional.
+# The payment-account-id param is required, the amount and fee-currency params are optional.
 $BISQ_HOME/bisq-cli --password=xyz --port=9998 takeoffer \
     --offer-id=83e8b2e2-51b6-4f39-a748-3ebd29c22aea \
     --payment-account-id=fe20cdbd-22be-4b8a-a4b6-d2608ff09d6e \
+    --amount=0.08 \
     --fee-currency=BTC
